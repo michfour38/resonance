dcd1ce776a12581a2fa0babe64790c338e0acf35
Michelle Fourie <michellefourie38@gmail.com>
2026-04-12 18:12:44 +0200

    disable middleware completely

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,1 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-
-const isPublicRoute = createRouteMatcher([
-  "/",
-  "/sign-in(.*)",
-  "/sign-up(.*)",
-  "/oremea(.*)",
-]);
-
-export default clerkMiddleware((auth, req) => {
-  if (!isPublicRoute(req)) {
-    auth().protect();
-  }
-});
-
-export const config = {
-  matcher: ["/((?!_next|.*\\..*).*)"],
-};
+export default function middleware() {}
