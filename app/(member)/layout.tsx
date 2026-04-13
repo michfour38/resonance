3f2c0be06562c41e2f770adf7514d93cdb18562b
Michelle Fourie <michellefourie38@gmail.com>
2026-04-13 07:56:55 +0200

    temporarily disable member layout auth

--- app/(member)/layout.tsx
+++ app/(member)/layout.tsx
@@ -1,16 +1,7 @@
-import { currentUser } from "@clerk/nextjs/server";
-import { redirect } from "next/navigation";
-
 export default async function MemberLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const user = await currentUser();
-
-  if (!user) {
-    redirect("/sign-in");
-  }
-
   return <div className="min-h-screen text-white">{children}</div>;
 }
