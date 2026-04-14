f762c8959f209b8e74867eaca416071cfbd7f5af
Michelle Fourie <michellefourie38@gmail.com>
2026-04-14 10:54:45 +0200

    add clerk middleware

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,20 @@
-export default function middleware() {}
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/",
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (!isPublicRoute(req)) {
+    auth().protect();
+  }
+});
+
+export const config = {
+  matcher: [
+    "/((?!_next|.*\\..*).*)",
+    "/(api|trpc)(.*)",
+  ],
+};
