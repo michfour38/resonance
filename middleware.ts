fe546a659e2dbd342ada0d55d6e08ed4c59c7c4d
Michelle Fourie <michellefourie38@gmail.com>
2026-05-11 16:52:00 +0200

    Unify 2Q and Mirror display logic

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,8 @@ const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
   "/sign-up(.*)",
   "/",
+  "/entry-mirror(.*)",
+  "/api/entry-mirror(.*)",
 ]);
 
 export default clerkMiddleware((auth, req) => {
@@ -13,8 +15,5 @@ export default clerkMiddleware((auth, req) => {
 });
 
 export const config = {
-  matcher: [
-    "/((?!_next|.*\\..*).*)",
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!_next|.*\\..*).*)", "/(api|trpc)(.*)"],
 };
