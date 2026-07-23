34ed4671c95ba888c0d3d7b54959b4920a2c2ff3
michfour38 <michellefourie38@gmail.com>
2026-07-23 13:16:54 +0200

    Remove empty tRPC root router

--- server/routers/_app.ts
+++
@@ -1,9 +0,0 @@
-// server/routers/_app.ts
-// Root tRPC router.
-// No active application routers are currently registered.
-
-import { router } from "../trpc";
-
-export const appRouter = router({});
-
-export type AppRouter = typeof appRouter;
