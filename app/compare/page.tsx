35b190a13bbd4078b9e888d3a6c678e9522e73f0
michfour38 <michellefourie38@gmail.com>
2026-07-24 17:02:13 +0200

    Limit compare page to public products

--- app/compare/page.tsx
+++ app/compare/page.tsx
@@ -5,9 +5,7 @@ import { useState } from "react";
 import { SiteShell } from "@/components/site/site-shell";
 
 import { CompareCompass } from "@/components/site/sections/compare-compass";
-import { CompareCurrent } from "@/components/site/sections/compare-current";
 import { CompareFinalGuidance } from "@/components/site/sections/compare-final-guidance";
-import { CompareHarmonize } from "@/components/site/sections/compare-harmonize";
 import { CompareHero } from "@/components/site/sections/compare-hero";
 import { CompareResonance } from "@/components/site/sections/compare-resonance";
 
@@ -23,10 +21,6 @@ export default function ComparePage() {
       <CompareResonance mode={mode} />
 
       <CompareCompass mode={mode} />
-
-      <CompareHarmonize mode={mode} />
-
-      <CompareCurrent mode={mode} />
 
       <CompareFinalGuidance />
     </SiteShell>
